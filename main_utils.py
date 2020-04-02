cc7e08c184b3053b3170086159e53ea5c1459063
Nathan <vandermo@ualberta.ca>
2020-04-02 10:46:35 +0200

    added extract_attributions

--- main_utils.py
+++ main_utils.py
@@ -17,3 +17,41 @@ def import_attribution_doc(filename):
     df = pd.read_csv(filename, sep='\t',names=headers)
     return df
 
+def extract_attributions(attribution_df):
+    '''
+    Takes a pandas df containing information from an attribution corpus file and returns
+    a list of attributions in the form of dictionaries of span tuples.
+    
+    :param attribution_df: a pandas df with info from an attribution file
+    
+    :returns attributions: a list of dictionaries of span tuples. An attribution consists of a source,
+        a cue, and a content, and each of these has an associated span (start and end indices). The
+        span indices represent the location of the source, cue or content in the df. "s" is the first
+        df row of the span, "e" is the line in the df after the span ends (use range(s, e) to traverse the span)
+        attributions = [{"SOURCE": (s, e), "CUE": (s, e), "CONTENT": (s, e)}, ...]
+    '''
+    attribution_info = attribution_df["attribution"]
+    num_attributions = len(attribution_info[0].split(" "))
+    attributions = [] 
+    attribution_parts = ["SOURCE", "CUE", "CONTENT"]
+    for i in range(num_attributions):
+        attributions.append({"SOURCE": None, "CUE": None, "CONTENT": None})
+    
+    for att_index in range(num_attributions):
+        for attribution_part in attribution_parts:
+            start_index = None
+            for i in range(len(attribution_info)):
+                attribution_string = attribution_info[i].split(" ")[att_index].split("-")
+                if attribution_string == ["_"] or attribution_string[1] != attribution_part:
+                    # Not inside a span
+                    if start_index != None:
+                        # Found the end of a span we were in
+                        assert attributions[att_index][att_part] == None, f"multi-part {att_part}? Line {i}"
+                        attributions[att_index][att_part] = (start_index, i)
+                        start_index = None
+                else:
+                    IOB_label, att_part = attribution_string[0], attribution_string[1]
+                    if att_part == attribution_part and IOB_label == "B":
+                        start_index = i
+            
+    return attributions        
