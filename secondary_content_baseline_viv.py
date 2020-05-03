7124bd926b6cfcf7e473af7317e894fa5032dba9
vivdenx <vivian.claes@live.nl>
2020-05-03 16:31:27 +0200

    Complete baseline for content spans (indirect quotations)

--- secondary_content_baseline_viv.py
+++ secondary_content_baseline_viv.py
@@ -1,17 +1,24 @@
-import pandas as pd
-import numpy as np
 from collections import defaultdict
 
+import pandas as pd
+
+# Define necessary filepaths.
 parc_filepath = '../2020_NLP/parc_features/parc_dev_features.tsv'
 polnear_filepath = '../2020_NLP/polnear_features/polnear_dev_features.tsv'
 
 
-def get_df(tsv_filepath):
+def get_candidate_indices(tsv_filepath):
+    """
+    Get a dictionary of all candidate indices from a tsv-file.
+
+    :param tsv_filepath: Path to a tsv file.
+    :return: content_dictionary (a dictionary with the indices of the content spans,
+                formatted as a tuple (filename, sent_no) as the key and a list of the content span indices as the value)
+             content_df (a dataframe that only contains the information of the content spans)
+    """
+    # Read in the tsv-data.
     df = pd.read_csv(tsv_filepath, delimiter='\t', index_col=0, low_memory=False)
-    return df
 
-
-def get_candidate_indices(df):
     candidate_indices = list()
 
     # List of tuples with whether (1) there is a quotation mark in the sentence and
@@ -29,6 +36,7 @@ def get_candidate_indices(df):
     content_df = df.loc[candidate_indices]
     content_dictionary = defaultdict(list)
 
+    # Add tuple (filename, sent_num) to the dictionary keys with index as value.
     for filename, sent_num, index in zip(content_df.filename, content_df.sentence_number, content_df.index):
         content_dictionary[filename, sent_num].append(index)
 
@@ -36,8 +44,20 @@ def get_candidate_indices(df):
 
 
 def get_cue_from_spans(content_dictionary, content_df):
+    """
+    Get a dictionary of all cues by looping over the content_df with contains only the necessary data.
+
+    :param content_dictionary: dictionary with a tuple of (filename, sentence_num) as key and a list of indices
+        that refer to the content as the value.
+    :param content_df: DataFrame that contains only the relevant data (sentences with no quotation marks, but
+        that do contain a cue.
+    :return: cues_dictionary (dictionary of cue indices, in the same format as the content_dictionary with tuples
+        (filename, sentence_num) as keys and a list of cue indices as the values. Often, however, there is only one
+        cue in the sentence, but in order to take into account all cases, this is presented as a list.
+    """
     cues_dictionary = defaultdict(list)
 
+    # Loop over the content_dictionary and the content_df to extract the cue labels.
     for (filename, sent_num), index in content_dictionary.items():
         for index, cue_label in zip(content_df.index, content_df.cue_label):
             if cue_label == 1:
@@ -47,10 +67,25 @@ def get_cue_from_spans(content_dictionary, content_df):
 
 
 def split_spans_on_cue(content_dictionary, cue_dictionary):
+    """
+    Split the content spans that were defined previously in content_dictionary on the cues found in cue_dictionary
+        to get a list of indices that appear before the cue and after the cue. This is valuable for later checking
+        which of these two list is longest.
+
+    :param content_dictionary: nested dictionary with a tuple of (filename, sentence_num) as key and a list of content
+        indices as value.
+    :param cue_dictionary: nested dictionary with a tuple of (filename, sentence_num) as key and a list of cue
+        indices as value.
+    :return: main_dictionary (a nested dictionary with tuples of (filename, sentence_num) as key and "before" and
+        "after" as nested keys that both contain a list of indices.
+    """
     main_dictionary = dict()
 
+    # Loop over content_dictionary and cue_dictionary to get the tuples and lists of indices.
     for content_tuple, content_indices in content_dictionary.items():
         for cue_tuple, cue_index in cue_dictionary.items():
+            # Check if the tuples are the same for content and cue and if they are define a before and after list,
+            # a main_dictionary with the tuple as the key and define cue_encountered as 0.
             if content_tuple == cue_tuple:
                 main_dictionary[content_tuple] = dict()
                 before = list()
@@ -58,24 +93,51 @@ def split_spans_on_cue(content_dictionary, cue_dictionary):
                 cue_encountered = 0
 
                 for index in content_indices:
+                    # If the index is a cue, set cue_encounterd to 1.
                     if index in cue_index:
                         cue_encountered = 1
                         continue
+                    # If no cue is encountered yet, add the index to the 'before' list.
                     if cue_encountered == 0:
                         before.append(index)
+                    # If a cue has been encountered previously, add the index to the 'after' list instead.
                     elif cue_encountered == 1:
                         after.append(index)
+
+                # Add both the 'before' and 'after' indices list to their respective (filename, sentence_num) tuple
+                # in the main dictionary.
                 main_dictionary[content_tuple]["before"] = before
                 main_dictionary[content_tuple]["after"] = after
 
     return main_dictionary
 
 
+def create_baseline_dictionary(main_dictionary):
+    """
+    Create a baseline dictionary based on the main_dictionary with before and after indices. The baseline dictionary
+    takes a list of indices that appear either before or after the cue, depending on which one is the longest.
+    This list is meant to indicate the content span.
+
+    :param main_dictionary: nested dictionary with a tuple of (filepath, sentence_num) as key and two lists with
+        indices for 'before' and 'after' the cue.
+    :return: main_dictionary (a dictionary with a tuple of (filepath, sentence_num) as key and a list with indices
+        that indicate the longest span either before or after the cue, which are meant to indicate the content.
+    """
+    # Loop over the entries in the main_dictionary to check whether the 'before' or 'after span is longer
+    # Whichever is longer is added to the dictionary as the value, with (filepath, sentence_num) as key.
+    for tuple, entry in main_dictionary.items():
+        if len(entry["before"]) > len(entry["after"]):
+            main_dictionary[tuple] = entry["before"]
+        else:
+            main_dictionary[tuple] = entry["after"]
+    return main_dictionary
+
+
 def run():
-    df = get_df(parc_filepath)
-    content_dictionary, content_df = get_candidate_indices(df)
+    content_dictionary, content_df = get_candidate_indices(parc_filepath)
     cues_dictionary = get_cue_from_spans(content_dictionary, content_df)
     main_dictionary = split_spans_on_cue(content_dictionary, cues_dictionary)
+    baseline_dictionary = create_baseline_dictionary(main_dictionary)
 
 
 if __name__ == "__main__":
